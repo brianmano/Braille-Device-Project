f83d43e5a462b6fadfb5421bc901f8c7c39a6d1b
Brian Manomaisupat <brian.manomaisupat@gmail.com>
2024-03-05 00:08:04 -0500

    added basic UI to take, scan and see images

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from components.image_reading import *
-from components.image_taking import *
+from components.image_reading import image_reading
+from components.image_taking import image_taking
+from PIL import ImageTk, Image as PILImage
+from tkinter import *
+import customtkinter
 
-image_taking()
+class main_menu(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Braille Translation Device")
+        self.geometry("500x800")
+        self._create_function_screen()
 
-results = image_reading('./images/temp.png')
-print(results)
+    def _create_function_screen(self):
+
+        customtkinter.CTkLabel(self, text="Braille Translation Device").place(relx=0.5, rely=0.2, anchor=CENTER)
+
+        customtkinter.CTkButton(self, text="Take Image", command=self._take_image).place(relx=0.5, rely=0.4, anchor=CENTER)
+
+        customtkinter.CTkButton(self, text="Read Image", command=self._read_image).place(relx=0.5, rely=0.6, anchor=CENTER)
+
+    def _take_image(self):
+        image_taking()
+        customtkinter.CTkLabel(self, text = " ", image=self._load_image('./images/temp.png')).place(relx=0.5, rely=0.8, anchor=CENTER)
+    def _read_image(self):
+        results = image_reading('./images/temp.png')
+        print(results)
+
+    def _load_image(self, image_path):
+        img = PILImage.open(image_path)
+        img = img.resize((300, 200), PILImage.NEAREST)
+        return ImageTk.PhotoImage(img)
+
+# Create an instance of the main_menu class
+if __name__ == "__main__":
+    app = main_menu()
+    app.mainloop()
