e4086583d358078318c31f7cf1c2002dab6989ff
clara <clarawong20@gmail.com>
2024-03-09 18:11:00 -0500

    hardcoded servo commands for digits 0-9

--- UI/app.py
+++ UI/app.py
@@ -3,6 +3,7 @@ from components.image_taking import image_taking
 from PIL import ImageTk, Image as PILImage
 from tkinter import *
 import customtkinter
+from servo_control import ServoControl
 
 class main_menu(customtkinter.CTk):
     def __init__(self):
@@ -22,14 +23,18 @@ class main_menu(customtkinter.CTk):
     def _take_image(self):
         image_taking()
         customtkinter.CTkLabel(self, text = " ", image=self._load_image('./UI/images/temp.png')).place(relx=0.5, rely=0.8, anchor=CENTER)
+    
     def _read_image(self):
         results = image_reading('./UI/images/temp.png')
         customtkinter.CTkLabel(self, text=results).place(relx=0.5, rely=0.95, anchor=CENTER)
+        servo_control = ServoControl()
+        servo_control.control_servos(results)
+        servo_control.close_connection()
 
     def _load_image(self, image_path):
         img = PILImage.open(image_path)
         img = img.resize((300, 200), PILImage.NEAREST)
-        return ImageTk.PhotoImage(img)
+        return ImageTk.PhotoImage(img)        
 
 # Create an instance of the main_menu class
 if __name__ == "__main__":
---
+++ UI/servo_control.py
@@ -0,0 +1,54 @@
+import serial 
+import time
+
+
+class ServoControl: 
+    #'COMx' is our Arduino's port, replace later
+    def __init__(self, port='COMx', baud_rate=9600):
+        self.ser = serial.Serial(port, baud_rate)
+        time.sleep(2)
+
+    def send_command(self, command):
+        self.ser.write(str(command).encode())
+        #allow arduino to process the command
+        time.sleep(1)
+ 
+    def control_servos(self, text):
+        if '0' in text:
+            self.send_command(2)
+            self.send_command(4)
+            self.send_command(5)
+        elif '1' in text:
+            self.send_command(1)
+        elif '2' in text:
+            self.send_command(1)
+            self.send_command(2)
+        elif '3' in text:
+            self.send_command(1)
+            self.send_command(4)
+        elif '4' in text:
+            self.send_command(1)
+            self.send_command(4)
+            self.send_command(5)
+        elif '5' in text:
+            self.send_command(1)
+            self.send_command(5)
+        elif '6' in text:
+            self.send_command(1)
+            self.send_command(2)
+            self.send_command(4)
+        elif '7' in text:
+            self.send_command(1)
+            self.send_command(2)
+            self.send_command(4)
+            self.send_command(5)
+        elif '8' in text:
+            self.send_command(1)
+            self.send_command(2)
+            self.send_command(5)
+        else:
+            self.send_command(2)
+            self.send_command(4)
+
+    def close_connection(self):
+        self.ser.close()
